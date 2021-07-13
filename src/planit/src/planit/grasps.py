6a1c1a2aef09d1776bf68dfb2cf821ecf7ceb5d9
Daniel Nakhimovich <dnahimov@gmail.com>
2021-07-13 01:13:55 -0400

    placement function test

--- src/planit/src/planit/grasps.py
+++ src/planit/src/planit/grasps.py
@@ -15,7 +15,7 @@ class Grasps:
         else:
             return list_to_pose([0, 0, 0, 0, 0, 0])
 
-    def get_simple_grasps(self, name, ax=None, ay=None, az=None, subframe=None):
+    def get_simple_grasps(self, name, off_dist, abs_off=(0, 0, 0.04), subframe=None):
         # scene.get_known_object_names()
         # scene.get_known_object_poses()
         frame_name = name if subframe is None else name + '/' + subframe
@@ -25,20 +25,21 @@ class Grasps:
         else:
             absolute = pose_msg2homogeneous(self.scene.get_object_poses([name])[name])
 
+        obj_off = translation_matrix((0, 0, -off_dist))
+        tab_off = translation_matrix(abs_off)
+
         poses = []
         angles = (-tau / 4, 0, tau / 4, tau / 2)
         for x in angles[::2]:
             for y in angles[::2]:
                 for z in angles:
-                    obj_off = translation_matrix((0, 0, -0.05))
-                    tab_off = translation_matrix((0, 0, 0.04))
                     relative = euler_matrix(x, y, z)
                     trans, rot = homogeneous2translation_rpy(concatenate_matrices(absolute, tab_off, relative, obj_off))
                     poses.append(trans + rot)
 
         return poses
 
-    def get_simple_placements(self, pose, frame_name=None, ax=None, ay=None, az=None):
+    def get_simple_placements(self, obj_name, pose, abs_off=None, frame_name=None):
         if frame_name:
             if '/' in frame_name:
                 name, frame = frame_name.split('/')[0]
@@ -50,13 +51,18 @@ class Grasps:
         else:
             absolute = translation_matrix(pose)
 
+        if abs_off:
+            offset = translation_matrix(abs_off)
+        else:
+            offset = identity_matrix()
+
         poses = []
         angles = (-tau / 4, 0, tau / 4, tau / 2)
         for x in angles:
             for y in angles:
                 for z in angles:
                     relative = euler_matrix(x, y, z)
-                    trans, rot = homogeneous2translation_rpy(concatenate_matrices(absolute, relative))
+                    trans, rot = homogeneous2translation_rpy(concatenate_matrices(absolute, offset, relative))
                     poses.append(trans + rot)
 
         return poses
