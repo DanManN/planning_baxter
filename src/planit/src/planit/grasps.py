e619fa6578d6099e9d80057d63dd2b552f967e5c
Daniel Nakhimovich <danielnak@tutanota.com>
2022-08-17 12:20:39 -0400

    adjusted grasp

--- src/planit/src/planit/grasps.py
+++ src/planit/src/planit/grasps.py
@@ -69,25 +69,25 @@ class Grasps:
             if sx < gw:
                 noz = nearOdd(sz / (gw * 1.5))
                 # for z in np.linspace(-(noz - 1) / (2 * noz), (noz - 1) / (2 * noz), noz):
-                for z in np.linspace(-0.4, 0.4, res):
+                for z in np.linspace(-0.1, 0.3, res):
                     grasps.append([[0, sy / 2, z * sz], horz[3]])  # right
                     grasps.append([[0, sy / 2, z * sz], horz[9]])  # right
                     grasps.append([[0, -sy / 2, z * sz], horz[5]])  # left
                     grasps.append([[0, -sy / 2, z * sz], horz[11]])  # left
                 noy = nearOdd(sy / gw)
                 # for y in np.linspace(-(noy - 1) / (2 * noy), (noy - 1) / (2 * noy), noy):
-                for y in np.linspace(-0.4, 0.4, res):
+                for y in np.linspace(-0.2, 0.2, res):
                     grasps.append([[0, y * sy, sz / 2], vert[1]])  # top
                     grasps.append([[0, y * sy, sz / 2], vert[3]])  # top
             if sy < gw:
                 noz = nearOdd(sz / gw)
                 # for z in np.linspace(-(noz - 1) / (2 * noz), (noz - 1) / (2 * noz), noz):
-                for z in np.linspace(-0.4, 0.4, res):
+                for z in np.linspace(-0.1, 0.3, res):
                     grasps.append([[-sx / 2, 0, z * sz], horz[4]])  # front
                     grasps.append([[-sx / 2, 0, z * sz], horz[10]])  # front
                 nox = nearOdd(sx / gw)
                 # for x in np.linspace(-(nox - 1) / (2 * nox), (nox - 1) / (2 * nox), nox):
-                for x in np.linspace(-0.4, 0.4, res):
+                for x in np.linspace(-0.2, 0.2, res):
                     grasps.append([[x * sx, 0, sz / 2], vert[0]])  # top
                     grasps.append([[x * sx, 0, sz / 2], vert[2]])  # top
 
@@ -96,8 +96,9 @@ class Grasps:
             h, r = shape.dimensions
             noz = nearOdd(h / (gw))
             # for z in np.linspace(-(noz - 1) / (2 * noz), (noz - 1) / (2 * noz), noz):
-            for z in np.linspace(-0.4, 0.4, res):
+            for z in np.linspace(0.0, 0.3, hres):
                 grasps += [[(0, 0, z * h), o] for o in vert]
+            for z in np.linspace(-0.1, 0.3, hres):
                 grasps += [
                     [(0, 0, z * h), o]
                     for o in horz[hres * ((res - 1) // 4):hres * ((res + 3) // 4)]
